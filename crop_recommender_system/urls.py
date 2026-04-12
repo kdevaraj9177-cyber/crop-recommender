f67b77f4387e067625e8ed88929982ab4ca0cfc1
Deva Raj <kdevaraj9177@@gmail.com>
2026-04-12 21:59:16 +0530

    Fixed static files, images and background issue

--- crop_recommender_system/urls.py
+++ crop_recommender_system/urls.py
@@ -50,10 +50,9 @@ path(
 
 
 urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
+    settings.STATIC_URL,
+    document_root=settings.STATICFILES_DIRS[0]
 )
-
 
 
 
